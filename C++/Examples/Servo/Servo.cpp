c8d2e15aa264a2fc722416778f3cd3f88defb631
cbz5012 <connorzaleski157@gmail.com>
2017-02-22 19:15:00 -0500

    Update Servo.cpp

--- C++/Examples/Servo/Servo.cpp
+++ C++/Examples/Servo/Servo.cpp
@@ -21,7 +21,34 @@ sudo ./Servo
 #define SERVO_MIN 1.250 /*mS*/
 #define SERVO_MAX 1.750 /*mS*/
 
-int main()
+#include <unistd.h>
+#include <cstdio>
+
+#include <Navio/RCInput.h>
+#include "Navio/Util.h"
+
+int main(int argc, char *argv[])
+{
+    RCInput rcin{};
+
+    if (check_apm()) {
+        return 1;
+    }
+
+    rcin.init();
+
+    while (true) 
+    {
+        int period = rcin.read(2);
+        printf("%d\n", period);
+        
+        sleep(1);
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
 {
     PWM pwm;
 
